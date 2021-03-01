24f58dc2b2de21bc5f3f11ab9d4465f907fb1d61
onestravel <server@onestravel.cn>
2021-03-01 10:57:30 +0800

    buildSrc

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,7 +1,9 @@
 object Version{
-
+    const val appcompat="1.0.0"
+    const val constraintlayout="1.1.3"
 }
 
 object Deps{
-
+const val appcompat="androidx.appcompat:appcompat:${Version.appcompat}"
+const val constraintlayout="androidx.constraintlayout:constraintlayout:${Version.constraintlayout}"
 }
